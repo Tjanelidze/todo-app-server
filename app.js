9788fdccc7ad3b48a43a6b282de20fe74363f332
Tedo <tedojanelidze7@gmail.com>
2024-06-09 15:13:39 +0400

    added cors

--- app.js
+++ app.js
@@ -2,9 +2,16 @@ import express from 'express';
 import userRouter from './routes/userRoutes.js';
 import todoRouter from './routes/todoRoutes.js';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 const app = express();
 
+const corsOptions = {
+  origin: '*',
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
 
