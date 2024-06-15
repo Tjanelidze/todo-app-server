977b21f5e3f95845036182ef3d2d0cc50721725e
Tedo <tedojanelidze7@gmail.com>
2024-06-15 15:17:04 +0400

    changed acces control for client

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ const corsOptions = {
   optionsSuccessStatus: 200,
 };
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(cors(corsOptions));
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
