0a89e682c74ce740bc8c0f48f37c16f197bf05c8
Tedo <tedojanelidze7@gmail.com>
2024-06-24 11:14:40 +0400

    modified getAllTodos so that it gets only current users todos

--- api/controllers/todosController.js
+++ api/controllers/todosController.js
@@ -27,7 +27,9 @@ const createTodo = async (req, res, next) => {
 
 const getAllTodos = async (req, res, next) => {
   try {
-    const todos = await Todo.find();
+    const todos = await Todo.find({ user: req.user.id }).sort({
+      createdAt: -1,
+    });
     res.status(200).json(todos);
   } catch (error) {
     res.status(500).json({ error: error.message });
